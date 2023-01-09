01d43757272f6b2135e985fdd2bd4fc9fe4c37a5
wwrrpp-HIT <weirp.hit@gmail.com>
2023-01-09 14:42:22 +0800

    第一版

--- configuration_discribe.py
+++ configuration_discribe.py
@@ -2,6 +2,9 @@ import copy
 
 import numpy as np
 import networkx as nx
+from typing import List
+
+import module as md
 
 config_T = {'config': 'config_T',
             'module_num': 5,
@@ -52,6 +55,15 @@ class Configuration:
         self.module_location = config.get('module_location')  # 组成构型的各模块位置
         self.module_adjacency = config.get('module_adjacency')  # 组成构型的各模块连接关系
         self.module_edges = config.get('module_edges')
+        self.generate_modules()
+
+    def generate_modules(self):
+        """构造参与构型的模块群"""
+        modules = []
+        for _ in range(self.module_num):
+            module = md.Module()
+            modules.append(module)
+        return modules
 
     def find_root(self):
         """
@@ -82,10 +94,15 @@ class Configuration:
                 copy_edges = self.module_edges.copy()
             else:
                 copy_edges = self.module_edges.copy()
-        return root, height
+        # return root, height
+        # 只输出第一个根节点序号
+        return [root[0]]
 
-    def node_children_in_tree(self, root):
-        neighbors = [list() for i in range(self.module_num)]
+    def node_children_in_tree(self, root) -> (List[List[int]]):
+        """输出以root为根的构型，各节点的子节点id。
+            e.g. [[], [0, 2], [], [1], [3, 6], [], [5, 7], []]
+        """
+        neighbors = [list() for _ in range(self.module_num)]
         module_set = [x for x in range(self.module_num)]
         for start, end in self.module_edges:
             neighbors[start].append(end)
@@ -93,8 +110,7 @@ class Configuration:
         children = list(copy.deepcopy(neighbors))
         root_neighbors = neighbors[root]
         root_children = root_neighbors
-        viewed_nodes = []
-        viewed_nodes.append(root)
+        viewed_nodes = [root]
         module_set.remove(root)
         current_nodes = root_children
         res = []
@@ -111,20 +127,31 @@ class Configuration:
             current_nodes = res  # current_node的更新按照上一轮检测的顺序来的，可能会存在问题
         return children
 
-    def _connect_position(self, module, neighbors):
+    def _connect_position(self, module, root):
         """'module_location': [(1, 1), (1, 2), (1, 3), (2, 2), (3, 2), (4, 1), (4, 2), (4, 3)]"""
         connect_position = []
-        for neighbor in neighbors:  # neighbors中包含了前一节点，需去掉
-            if module[0] - neighbor[0] != 0 and module[1] - neighbor[1] != 0:
-                return f"they are not neighbors，{module[0]} and {neighbor[0]}"
+
+        module_location = self.module_location[module]
+
+        children = self.node_children_in_tree(root)
+        module_children = children[module]
+
+        for child in module_children:
+            if module_location[0] - self.module_location[child][0] != 0 and module_location[1] - \
+                    self.module_location[child][1] != 0:
+                return f"they are not neighbors，{module} and {child}"
             else:
-                if module[0] - neighbor[0] == 0 and module[1] - neighbor[1] == 1:
+                if module_location[0] - self.module_location[child][0] == 0 and module_location[1] - \
+                        self.module_location[child][1] == 1:
                     connect_position.append(str('L'))
-                elif module[0] - neighbor[0] == 0 and module[1] - neighbor[1] == -1:
+                elif module_location[0] - self.module_location[child][0] == 0 and module_location[1] - \
+                        self.module_location[child][1] == -1:
                     connect_position.append('R')
-                elif module[0] - neighbor[0] == 1 and module[1] - neighbor[1] == 0:
+                elif module_location[0] - self.module_location[child][0] == 1 and module_location[1] - \
+                        self.module_location[child][1] == 0:
                     connect_position.append('F')
-                elif module[0] - neighbor[0] == -1 and module[1] - neighbor[1] == 0:
+                elif module_location[0] - self.module_location[child][0] == -1 and module_location[1] - \
+                        self.module_location[child][1] == 0:
                     connect_position.append('F')
         return connect_position
 
@@ -173,13 +200,12 @@ class Configuration:
         return leaves
 
 
-t = Configuration(config_T)
-cross = Configuration(config_cross)
-h = Configuration(config_H)
-print(h.find_root())
-print(h.node_children_in_tree(4))
-h_children = h.node_children_in_tree(4)
-for i in range(h.module_num):
-    for j in h_children[i]:
-        child_location = [h.module_location[j]]
-        print(h._connect_position(h.module_location[i], child_location),f'{i},{j}')
+# t = Configuration(config_T)
+# cross = Configuration(config_cross)
+# h = Configuration(config_H)
+# print(h.generate_modules())
+# print(h.find_root())
+# print(h.node_children_in_tree(4))
+# h_children = h.node_children_in_tree(4)
+# for i in range(h.module_num):
+#     print(h._connect_position(i, 3), f'{i}')
---
+++ mapping_planning.py
@@ -0,0 +1,79 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+from configuration_discribe import Configuration
+
+config_shizi = {'module_adjacency':
+                    [[0, 3, 0, 0, 0, 0, 0],
+                     [1, 0, 2, 3, 4, 0, 0],
+                     [0, 4, 0, 0, 0, 0, 0],
+                     [0, 1, 0, 0, 0, 3, 0],
+                     [0, 4, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 1, 0, 0, 3],
+                     [0, 0, 0, 0, 0, 3, 0]],
+                'module_edges': [(0, 1), (1, 2), (1, 3), (1, 4), (3, 5), (5, 6)],
+                'module_num': 7,
+                }
+
+config_tu = {'module_adjacency':
+                 [[0, 1, 0, 0, 0, 0, 0],
+                  [1, 0, 3, 0, 0, 0, 0],
+                  [0, 1, 0, 2, 0, 4, 0],
+                  [0, 0, 4, 0, 3, 0, 0],
+                  [0, 0, 0, 2, 0, 0, 0],
+                  [0, 0, 2, 0, 0, 0, 3],
+                  [0, 0, 0, 0, 0, 1, 0]],
+             'module_edges': [(0, 1), (1, 2), (2, 3), (2, 5), (3, 4), (5, 6)],
+             'module_num': 7,
+             }
+
+
+class ConfigurationMapping:
+    def __init__(self, init_config, goal_config):
+        self.init_adjacency = init_config.get('module_adjacency')  # 组成构型的各模块连接关系
+        self.goal_adjacency = goal_config.get('module_adjacency')
+        self.init_edges = init_config.get('module_edges')
+        self.goal_edges = goal_config.get('module_edges')
+        self.init_config = Configuration(init_config)
+
+    def plot_goal_config(self):
+        x = np.array(self.goal_adjacency)
+        modules_num = len(x)
+        rootnode = self.init_config.find_root()
+        g = nx.Graph()
+        g.add_nodes_from(range(0, modules_num), color='blue')
+        g.add_edges_from(self.goal_edges)
+        g.add_node(rootnode[0], color='red')
+        print(nx.get_node_attributes(g, 'color'))
+        nx.draw(g, with_labels=True)
+        plt.show()
+
+
+config_T = {'config': 'config_T',
+            'module_num': 5,
+            'module_location': [(1, 1), (2, 1), (3, 1), (2, 2), (2, 3)],
+            'module_adjacency':
+                [[0, 1, 0, 0, 0],
+                 [1, 0, 1, 1, 0],
+                 [0, 1, 0, 0, 0],
+                 [0, 1, 0, 0, 1],
+                 [0, 0, 0, 1, 0]],
+            'module_edges': [(0, 1), (1, 2), (1, 3), (3, 4)],
+            }
+
+config_cross = {'config': 'config_cross',
+                'module_num': 7,
+                'module_location': [(1, 1), (1, 2), (1, 3), (1, 4), (2, 4), (0, 4), (1, 5)],
+                'module_adjacency':
+                    [[0, 1, 0, 0, 0, 0, 0],
+                     [1, 0, 1, 0, 0, 0, 0],
+                     [0, 1, 0, 1, 0, 0, 0],
+                     [0, 0, 1, 0, 1, 1, 1],
+                     [0, 0, 0, 1, 0, 0, 0],
+                     [0, 0, 0, 1, 0, 0, 0],
+                     [0, 0, 0, 1, 0, 0, 0]],
+                'module_edges': [(0, 1), (1, 2), (2, 3), (3, 4), (3, 5), (3, 6)],
+                }
+mapping = ConfigurationMapping(config_shizi, config_shizi)
+mapping.plot_goal_config()
